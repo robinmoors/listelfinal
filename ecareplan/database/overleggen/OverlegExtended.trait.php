25ef8d51c97adab48c1c479fbeca44adef0598bd
joris <joris@joris-PC.khlim.local>
2013-08-22 09:29:51 +0200

    conflicts resolved van gelijkaardige functienamen v2

--- ecareplan/database/overleggen/OverlegExtended.trait.php
+++ ecareplan/database/overleggen/OverlegExtended.trait.php
@@ -331,7 +331,7 @@ trait OverlegExtendedTrait {
 	 *
 	 * @return array
 	 */
-	public function toArray() {
+	public function toArrayExt() {
 		return array(
 			'uid'=>$this->getUid(),
 			'overleg_id'=>$this->getOverlegId(),
@@ -347,5 +347,84 @@ trait OverlegExtendedTrait {
 			'ambulant'=>$this->getAmbulant(),
 			'huisarts_belangrijk'=>$this->getHuisartsBelangrijk());
 	}
+        
+        public function assignByHashExt($result){
+            parent::assignByHash($result);
+            $this->setUid($result['uid']);
+            $this->setOverlegId($result['overleg_id']);
+            $this->setLocatieTekst($result['locatieTekst']);
+            $this->setTijdstip($result['tijdstip']);
+            $this->setAkkoortPatient($result['akkoord_patient']);
+            $this->setAanwezigPatient($result['aanwezig_patient']);
+            $this->setVertegenwoordiger($result['vertegenwoordiger']);
+            $this->setEvalNieuw($result['eval_nieuw']);
+            $this->setAfronddatum($result['afronddatum']);
+            $this->setVolgendeDatum($result['volgende_datum']);
+            $this->setVerklaringHuisarts($result['verklaring_huisarts']);
+            $this->setAmbulant($result['ambulant']);
+            $this->setHuisartsBelangrijk($result['huisarts_belangrijk']);
+        }
+        
+        
+        public static function getFieldNamesExt(){
+            return array(1=>'uid',
+                2=>'overleg_id',
+                3=>'locatieTekst',
+                4=>'tijdstip',
+                5=>'akkoord_patient',
+                6=>'aanwezig_patient',
+                7=>'vertegenwoordiger',
+                8=>'eval_nieuw',
+                9=>'afronddatum',
+                10=>'volgende_datum',
+                11=>'verklaring_huisarts',
+                12=>'ambulant',
+                13=>'huisarts_belangrijk');
+        }
+        
+        /*
+        public function insertIntoDatabaseExt(PDO $db){
+            $uid = $this->getUid();
+            $overlegid  = $this->getOverlegId();
+            $ombfactuur = $this->getLocatieTekst();
+            $ombactief = $this->getTijdstip();
+            $ombrangorde = $this->getAkkoortPatient();
+            $overlegid  = $this->getAanwezigPatient();
+            $ombfactuur = $this->getVertegenwoordiger();
+            $ombactief = $this->getEvalNieuw();
+            $ombrangorde = $this->getAfronddatum();
+            $overlegid  = $this->getVolgendeDatum();
+            $ombfactuur = $this->getVerklaringHuisarts();
+            $ombactief = $this->getAmbulant();
+            $ombrangorde = $this->getHuisartsBelangrijk();
+            
+            $query = 'INSERT INTO `overlegomb`
+                SET `uid` = :uid, `overleg_id`= :overleg_id, `omb_factuur`= :omb_factuur, `omb_actief`= :omb_actief, `omb_rangorde`= :omb_rangorde';
+            
+            try{
+                $statement= $db->prepare($query);
+                $statement->bindValue(':uid',$uid);
+                $statement->bindValue(':overleg_id',$overlegid);
+                $statement->bindValue(':omb_factuur',$ombfactuur);
+                $statement->bindValue(':omb_actief',$ombactief);
+                $statement->bindValue(':omb_rangorde',$ombrangorde);
+                $affected = $statement->execute();
+                if (false===$affected) {
+			$statement->closeCursor();
+			throw new Exception($statement->errorCode() . ':' . var_export($statement->errorInfo(), true), 0);
+		}
+		$lastInsertId=$db->lastInsertId();
+		if (false!==$lastInsertId) {
+			$this->setUid($lastInsertId);
+		}
+                $statement->closeCursor();
+            } catch (PDOException $e) {
+                $error_messsage = $e->getMessage();
+                //display
+            }
+            return $affected;
+        }
+         * */
+         
 }
 ?>
